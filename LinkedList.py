6b4b84294208cbb73ca789c339598e06f36850d9
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-23 22:28:47 +0300

    comment neccessary lines

--- LinkedList.py
+++ LinkedList.py
@@ -7,31 +7,40 @@ class ListNode():
 
 class LinkedList():
     def __init__(self):
-        self.head = None
+        self.head = None # This means we have an empty linked list at the start because the head is pointing to nothing
 
     def addNodeAtFirst(self):
-        inp = input("Enter value: ")
-        if self.head == None:
-            temp = ListNode(inp)
-            self.head = temp
-            temp.next = None
-        else:
-            head = self.head
-            self.head = ListNode(inp)
-            self.head.next = head
-
-    def addNodeAtEnd(self):
+        # Accept the value and create the node
         inp = input("Enter value: ")
         temp = ListNode(inp)
-        temp.next = None
+
+        # This checks if the linked list is empty or there are nodes
+        if self.head == None:
+            self.head = temp # Make the new node the head of the linked list
+            temp.next = None # Make the next of the new node point to nothing
+        else:
+            # This means there are already other nodes in the linked lilst
+            head = self.head
+            self.head = temp # Make the new node the head node
+            self.head.next = head # Make the previous node next to the new node
+
+    def addNodeAtEnd(self):
+        # Accept and create the new node
+        inp = input("Enter value: ")
+        temp = ListNode(inp)
+        temp.next = None # Make the next of the new node nothing
+
+        # This checks if the linked list has already other nodes
         if self.head == None:
             self.head = temp
         else:
             head = self.head
+            # Loop until you find the last node, this can be done by running a while loop 
+            # until you find that the next of the lase node is nothing
             while head.next != None:
                 head = head.next
 
-            head.next = temp
+            head.next = temp # Make the new node the last node
 
     def display(self):
         head = self.head
