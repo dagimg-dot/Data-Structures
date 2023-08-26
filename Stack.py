382e6e9a4fdcc265b16ca6543f5d00a85c74062a
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-26 13:10:24 +0300

    add push method

--- Stack.py
+++ Stack.py
@@ -3,4 +3,7 @@ from DataStructures import DataStructures
 class Stack(DataStructures):
     def __init__(self):
         super().__init__()
+
+    def push(self, value: any) -> None:
+        self._addNodeAtEnd(val=value)
     
