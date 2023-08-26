d1f49fb4634081329e21f225666c6ea81d102dc0
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-26 17:31:13 +0300

    add doc to push method

--- Stack.py
+++ Stack.py
@@ -1,12 +1,12 @@
 from DataStructures import DataStructures
 
+
 class Stack(DataStructures):
     def __init__(self):
         super().__init__()
 
     def push(self, value: any) -> None:
+        """
+        Push value to the top of the stack
+        """
         self._addNodeAtEnd(val=value)
-    
-    def pop(self) -> None:
-        self._deleteNodeAtEnd()
-    
