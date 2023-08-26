1d8a57b69fb90d308d0604a4594355dcd27b7ff4
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-26 17:31:55 +0300

    add pop method

--- Stack.py
+++ Stack.py
@@ -10,3 +10,23 @@ class Stack(DataStructures):
         Push value to the top of the stack
         """
         self._addNodeAtEnd(val=value)
+
+    def pop(self, index=-1) -> any:
+        """
+        Remove and return item at index (default last)\n\n
+        Raises IndexError if Stack is empty or index is out of range
+        """
+        size = self.size()
+        if size == 0:
+            raise IndexError
+
+        if index == -1 or index == size - 1:
+            last_node = self._getByIndex(size - 1)
+            value = last_node.val
+            self._deleteNodeAtEnd()
+            return value
+        else:
+            node = self._getByIndex(index)
+            value = node.val
+            self._deleteNodeAtMiddle(value)
+            return value
