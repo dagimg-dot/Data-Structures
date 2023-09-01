5ecc979dac864366ad615b1a8d31007843919c20
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-09-01 20:02:20 +0300

    add display method

--- DoubleLinkedList.py
+++ DoubleLinkedList.py
@@ -4,6 +4,24 @@ class ListNode():
         self.prev = None
         self.next = None
 
+
 class DoubleLinkedList():
     def __init__(self) -> None:
         self.head = None
+
+    def display(self) -> None:
+        # TODO: I have to know what "Print the values to a stream" means related to the terminal
+        """
+        Print the values to a stream
+        """
+        head = self.head
+        if head == None:  # If the head is None then the list is empty
+            print("[]")
+        else:
+            print("[", end='')
+            while head != None:  # Loop until the list is over, we know its over when the last node's next is None
+                print(head.val, end=', ') if head.next != None else print(
+                    head.val, end='')
+                head = head.next
+            print("]", end='')
+            print()
