31b30f52584462a8e2e4b01b0d0ac16ed2a956ed
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-09-16 22:03:22 +0300

    add maxDepth method

--- Tree.py
+++ Tree.py
@@ -13,3 +13,12 @@ class Tree():
 
     def __init__(self) -> None:
         self.root = None
+
+    def maxDepth(self, root: TreeNode) -> int:
+        """
+        Returns the maximum depth of the tree
+        """
+        if root == None:
+            return 0
+        else:
+            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
