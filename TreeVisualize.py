414f7e3a1450257d5f2f0ba27e57d2cdffb80326
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-09-18 16:59:51 +0300

    manage to create a function to generate a line of the tree at a time

--- TreeVisualize.py
+++ TreeVisualize.py
@@ -1,19 +1,3 @@
-space = " "
-
-output = """
-                            7
-             _______________|_______________
-            |                               |
-            9                               12
-      ______|______                   ______|______
-     |             |                 |             |
-     5             4                 2             3
- ____|____     ____|____
-|         |   |         |
-8         7
-"""
-
-
 def getSpace(numSpace):
     space = " "
     return space * numSpace
@@ -36,7 +20,7 @@ def getBinaryLine(numSpace, numUnderScore, spaceFromLeft, spaceBetween=0, quanti
 {getSpace(spaceFromLeft)}{getSpace(numSpace)}|{getSpace(numUnderScore * 2 + 1)}|
 """
         return binary_line
-
+    
 
 a = 6
 b = 3
@@ -46,11 +30,51 @@ x = f"""
 {c}
 {getBinaryLine(0, a, sfl)}
 {getSpace(sfl)}{9}{getSpace(a * 2 + 1)}{12}
-{getBinaryLine(0, a - b, sfl//2 - b + 5, (a * 2 - (a - b) * 2), 2)}
-{getSpace(sfl - (a - b + 1))}{9}{getSpace((a - b) * 2 + 1)}{1}{getSpace((a * 2 - (a - b) * 2) - 1)}{3}{getSpace((a - b) * 2 + 1)}{2}
+"""
+# {getBinaryLine(0, a - b, sfl//2 - b + 5, (a * 2 - (a - b) * 2), 2)}
+# {getSpace(sfl - (a - b + 1))}{9}{getSpace((a - b) * 2 + 1)}{1}{getSpace((a * 2 - (a - b) * 2) - 1)}{3}{getSpace((a - b) * 2 + 1)}{2}
+
+
+def getSpace(numSpace):
+    space = " "
+    return space * numSpace
+
+
+def getUnderScore(quantity):
+    return "_" * quantity
+
+# hypothetical function which takes a list as space number between what is written
+# for example 
+
+h1 = """
+    7
+ ___|___
+|       |
+9       12
 """
 
+# L1: [4], [7]
+# L2: [1], ["___|___"] -> this will be generated
+# L3: [0,7],["|","|"]
+# L4: [0,7],[9,12]
 
-# {getBinaryLine(3, 4, 2)}{getSpace(3)}{5}{getSpace(9)}{4}{getSpace(5)}{2}{getSpace(9)}{3}
+def lineGenerator(spaces, values):
+    final = ""
+    for i in range(len(spaces)):
+        final += f"{getSpace(spaces[i])}{values[i]}"
 
-print(x)
+    return final
+
+
+# print(lineGenerator([4], [7]))
+# print(lineGenerator([1], ["___|___"]))
+# print(lineGenerator([0,7],["|","|"]))
+# print(lineGenerator([0,7],[9,12]))
+
+print(lineGenerator([11], [4]))
+print(lineGenerator([5], ["______|______"]))
+print(lineGenerator([4,13],["|","|"]))
+print(lineGenerator([4,13],[7,8]))
+print(lineGenerator([1,7], ["___|___","___|___"]))
+print(lineGenerator([0,7,5,7],["|","|","|","|"]))
+print(lineGenerator([0,7,4,7],[9,12,5,2]))
