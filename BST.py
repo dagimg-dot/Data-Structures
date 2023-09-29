90ea9bc534a4b8e3ba3fbc0b1391143cb7a58033
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-09-29 20:37:09 +0300

    add insert method

--- BST.py
+++ BST.py
@@ -14,6 +14,35 @@ class BST():
     def __init__(self) -> None:
         self.root = None
 
+    def insert(self, value: any) -> bool:
+        """
+        Insert value into the binary search tree.
+
+        Return true if the element is inserted successfully.
+        """
+        new_treeNode = TreeNode(value)
+        if self.root == None:
+            self.root = new_treeNode
+        else:
+            parent = None
+            current = self.root
+            while current != None:
+                if value < current.val:
+                    parent = current
+                    current = current.left
+                elif value > current.val:
+                    parent = current
+                    current = current.right
+                else:
+                    return False
+
+            if value < parent.val:
+                parent.left = new_treeNode
+            else:
+                parent.right = new_treeNode
+
+        return True
+
     def height(self) -> int:
         """
         Returns the maximum depth of the tree
